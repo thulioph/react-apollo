bacf397490c215aa0e9c6eb8139cb6893377dfbe
Thulio Philipe <thulioph@gmail.com>
2018-01-02 17:49:51 -0300

    Updates the Link components with deleteLink method.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
 // ====
 
 class Link extends React.Component {
+    _deleteLink = async ({id}) => {
+        console.warn(id);
+    }
+
     render() {
         const { link } = this.props;
 
@@ -14,6 +21,11 @@ class Link extends React.Component {
                     <a href={link.url} target='_blank'>
                         {link.url}
                     </a>
+                </td>
+                <td>
+                    <button className="button is-danger" onClick={() => this._deleteLink(link)}>
+                        <i title="Remove" className="fa fa-trash-o" aria-hidden="true"></i>
+                    </button>
                 </td>
             </tr>
         )
